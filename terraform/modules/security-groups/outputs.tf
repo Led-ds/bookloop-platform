b4113cc41943816554f7a81efac5828cdc556ed4
alexsoares <alexsaosilva@gmail.com>
2026-07-04 16:51:21 -0300

    style: terraform fmt em outputs de vpc e security-groups

--- terraform/modules/security-groups/outputs.tf
+++ terraform/modules/security-groups/outputs.tf
@@ -1,2 +1,2 @@
 output "apprunner_connector_sg_id" { value = aws_security_group.apprunner_connector.id }
-output "rds_sg_id"                 { value = aws_security_group.rds.id }
+output "rds_sg_id" { value = aws_security_group.rds.id }
